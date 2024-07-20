11ecce1bb3ee615b7c4cb868016288dd24c7f0e5
Ishan Verma <83359975+Ishan-creed@users.noreply.github.com>
2024-07-21 04:09:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
+
 
 const app = express();
 
-mongoose.connect("mongodb+srv://myUser:user12345@cluster0.wk0dk.mongodb.net/manufacure?retryWrites=true&w=majority", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        console.log('Connected to MongoDB Atlas');
-    })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB Atlas:', error);
-    });
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('Connected to MongoDB Atlas');
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
+});
+
 
 app.use(cors());
 app.use(bodyParser.json());
